9e9fcb35d005d850b1ace14e479eae3ca3f59b86
dhall88 <dhall87@gmail.com>
2020-02-23 14:32:12 -0500

    troubleshooting backlight

--- newApp.js
+++ newApp.js
@@ -30,7 +30,7 @@ $(() => {
   scene.add(purpleLight);
 
   greenLight = new THREE.PointLight(0x32a852,1,450,1.7);
-  greenLight.position.set(100,500,300);
+  greenLight.position.set(300,300,200);
   scene.add(greenLight);
 
   lightning = new THREE.PointLight(0x3677ac,0,450,1.7);
